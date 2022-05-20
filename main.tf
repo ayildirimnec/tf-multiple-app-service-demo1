f232a4e6002717163a9abc55c3156e26a569e0d7
ayildirimnec <87165930+ayildirimnec@users.noreply.github.com>
2022-05-20 13:47:04 +1000

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,13 +22,13 @@ terraform {
 #}
 
 resource "azurerm_resource_group" "rg" {
-  name     = "${var.environment}rgmultipleappservicedemo"
+  name     = "${var.targetenvironmet}rgmultipleappservicedemo"
   location = "westus2"
 }
 
 resource "azurerm_app_service_plan" "app-plan-linux" {
   count = 3   
-  name                = "${var.environment}necdemoappsvc-${count.index}"
+  name                = "${var.targetenvironmet}necdemoappsvc-${count.index}"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   kind                = "Linux"
