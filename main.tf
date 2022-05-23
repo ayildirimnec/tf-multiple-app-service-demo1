dd646033abd4db2afc3bc4e32aeee42ffccf836e
ayildirimnec <87165930+ayildirimnec@users.noreply.github.com>
2022-05-23 12:06:05 +1000

    Update main.tf

--- main.tf
+++ main.tf
@@ -31,6 +31,7 @@ resource "azurerm_app_service_plan" "app-plan-linux" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   kind                = "Linux"
+  reserved            = true
 
   sku {
     tier = "Free"
