41d19efa58ffb0e8d46022a577dfd9c21e62b391
ayildirimnec <87165930+ayildirimnec@users.noreply.github.com>
2022-05-23 12:02:57 +1000

    Update main.tf

--- main.tf
+++ main.tf
@@ -31,7 +31,6 @@ resource "azurerm_app_service_plan" "app-plan-linux" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   kind                = "Linux"
-  reserved            = true
 
   sku {
     tier = "Free"
@@ -46,19 +45,8 @@ resource "azurerm_app_service" "example" {
   resource_group_name = azurerm_resource_group.rg.name
   app_service_plan_id = azurerm_app_service_plan.app-plan-linux.id
 
-  site_config {
-    dotnet_framework_version = "v4.0"
-    scm_type                 = "LocalGit"
-  }
-
   app_settings = {
     "SOME_KEY" = "some-value"
-  }
-
-  connection_string {
-    name  = "Database"
-    type  = "SQLServer"
-    value = "Server=some-server.mydomain.com;Integrated Security=SSPI"
   }
 }
 
